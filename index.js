8e0d66ca50be96379bc5a8ea536fd474ccd1cb92
Michael Dally <mddally@gmail.com>
2022-02-11 11:43:12 -0500

    commit tree 10d4396b424713c9c0154925e9e29acbdcde5274

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const user = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(user),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(data => addToDom(data))
+    .catch(error => addErrorToDom(error))
+}
+
+function addToDom(user){
+    console.log(user)
+    const id = document.createElement('h1');
+    id.textContent = user.id;
+    document.body.append(id);
+}
+
+function addErrorToDom(error){
+    const errorMessage = document.createElement('h2');
+    errorMessage.textContent = error.message;
+    document.body.append(errorMessage);
+}
